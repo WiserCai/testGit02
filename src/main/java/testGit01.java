103accafad1b7fccc7744b83eb06135c9aba9262
WellCai <wellcai120610@aliyun.com>
2020-06-14 18:04:04 +0800

    master v01

---
+++ src/main/java/testGit01.java
@@ -0,0 +1,11 @@
+/**
+ * @program: rep3
+ * @description(描述)
+ * @author: WellCai
+ * @create: 2020-06-14 18:02
+ **/
+public class testGit01 {
+    public static void main(String[] args) {
+        System.out.println("v01");
+    }
+}
