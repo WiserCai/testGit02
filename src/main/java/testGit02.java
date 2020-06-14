921a04f3fea90c7e448f53ed1cf8587715ac9ad8
WellCai <wellcai120610@aliyun.com>
2020-06-14 18:12:38 +0800

    开发了v02版

---
+++ src/main/java/testGit02.java
@@ -0,0 +1,12 @@
+/**
+ * @program: rep3
+ * @description(描述)
+ * @author: WellCai
+ * @create: 2020-06-14 18:11
+ **/
+public class testGit02 {
+    public static void main(String[] args) {
+        System.out.println("V02 ");
+
+    }
+}
